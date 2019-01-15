aad1b3b8fed4bbafb4f2550c7675f254a97cef6b
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-15 20:49:12 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -5,7 +5,7 @@ function callMe() {
   console.log("so call me");
   var lyric = "maybe";
   return lyric;
-  
+
 }
 
 
@@ -13,7 +13,7 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
